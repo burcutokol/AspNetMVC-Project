f0a7de7fe3411c68aa3ca48cb56eebbf1583c1ae
burcutokol <tokolburcu@gmail.com>
2023-12-18 13:31:06 +0300

    initalize mvc project

--- AspNetMVC/Models/Model/Contact.cs
+++ AspNetMVC/Models/Model/Contact.cs
@@ -13,17 +13,19 @@ namespace AspNetMVC.Models.Model
     {
         [Key]
         public int ContactId { get; set; }
-
+        [StringLength(150, ErrorMessage = "Adres maksimum 500 0karakter olabilir.")]
         public string Address { get; set; }
-
+        [StringLength(150, ErrorMessage = "Telefon numarası maksimum 50 karakter olabilir.")]
         public string TelNumber { get; set; }
-
+        [StringLength(150, ErrorMessage = "Fax maksimum 50 karakter olabilir.")]
         public string Fax { get; set; }
-
+        [StringLength(150, ErrorMessage = "Whatsapp maksimum 50 karakter olabilir.")]
         public string Whatsapp { get; set; }
-
+        [StringLength(150, ErrorMessage = "Facebook maksimum 50 karakter olabilir.")]
         public string Facebook { get; set; }
+        [StringLength(150, ErrorMessage = "Instagram maksimum 50 karakter olabilir.")]
         public string Instagram { get; set; }
+        [StringLength(150, ErrorMessage = "Twitter maksimum 50 karakter olabilir.")]
         public string Twitter { get; set; }
     }
 }
