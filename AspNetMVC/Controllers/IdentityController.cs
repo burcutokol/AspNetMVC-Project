c436644d6f0b7bc81e9bc9f357557470b1949c9f
burcutokol <tokolburcu@gmail.com>
2024-01-10 01:11:34 +0300

    site identity controller created

---
+++ AspNetMVC/Controllers/IdentityController.cs
@@ -0,0 +1,42 @@
+﻿using AspNetMVC.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class IdentityController : Controller
+    {
+        DbContextClass db = new DbContextClass();
+        // GET: Identity
+        public ActionResult Index()
+        {
+            return View(db.SiteIdentity.ToList());
+        }
+
+    
+        // GET: Identity/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Identity/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
